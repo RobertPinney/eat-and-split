b776d462d17997d4d018b07a4d76372fad1d340d
Robert <pinneyr@hotmail.com>
2023-11-22 16:20:39 +0100

    component setup

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+import "./app.css";
+
 const initialFriends = [
   {
     id: 118836,
@@ -37,6 +39,81 @@ function EatAndSplit() {
   );
 }
 
-function List() {}
-function FormFriend() {}
-function FormBill() {}
+function List() {
+  return (
+    <div>
+      <SideBar />
+    </div>
+  );
+}
+
+function SideBar() {
+  return (
+    <div>
+      <ul className="sidebar">
+        <li>
+          <img src="" alt="" />
+        </li>
+        <li>
+          <h3>Name</h3>
+        </li>
+        <li>
+          <p>Money owed</p>
+        </li>
+        <li>
+          <button className="button">Select</button>
+        </li>
+      </ul>
+    </div>
+  );
+}
+
+function FormFriend() {
+  return (
+    <div className="form-add-friend">
+      <span>
+        <emote></emote>
+        <p>Friend Name</p>
+        <input></input>
+      </span>
+      <span>
+        <emote></emote>
+        <p>Image URL</p>
+        <input></input>
+      </span>
+      <button className="button">Add</button>
+    </div>
+  );
+}
+
+function FormBill() {
+  return (
+    <div>
+      <h2>SPLIT A BILL WITH (NAME)</h2>
+      <span>
+        <emote></emote>
+        <label>Bill Value:</label>
+        <input type="text" />
+      </span>
+      <span>
+        <emote></emote>
+        <label>Your expense:</label>
+        <input type="text" />
+      </span>
+      <span>
+        <emote></emote>
+        <label>(Name) expense:</label>
+        <input type="text" />
+      </span>
+      <span>
+        <emote></emote>
+        <label>Who is paying the bill?</label>
+        <select name="" id="">
+          <option value=""></option>
+          <option value=""></option>
+          <option value=""></option>
+        </select>
+      </span>
+    </div>
+  );
+}
